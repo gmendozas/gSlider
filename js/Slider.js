bd81e4c7127731bfa4796be3450066880f5b226e
unknown <extsiscred02@EXTSISCRED02.compartamos.mx>
2014-06-12 18:59:48 -0500

    Modified to use configured instance of gSlider.

--- js/Slider.js
+++ js/Slider.js
@@ -1,232 +1,177 @@
+// Class image
 function Image(src, alt, link) {
 	this.src = src;
 	this.alt = alt;
 	this.link = link;
 };
-//var images = new Array();
-//var i = 0, speed = 2000;
-//var firstTime = true;
-
-$(document).ready(function() {	
-	/*images[0] = new Image("images/img4.jpg", "Imagen 4", "http://www.google.com");
-	images[1] = new Image("images/img5.jpg", "Imagen 5", "http://twitter.com");
-	images[2] = new Image("images/img6.jpg", "Imagen 6", "http://www.facebook.com");
-	images[3] = new Image("images/img7.jpg", "Imagen 7", "http://www.microsoft.com");
-
-	$("#bannerLink").hover(function() {
-			$(".bottomLabelImg").show();
-		},
-		function() {
-			$(".bottomLabelImg").hide();
-		}		
-	);
-	
-	$(".play").hover(
-		function(){
-			$(this).attr("src", "images/play_hover.png");
-		},
-		function(){
-			$(this).attr("src", "images/play.png");
-		}
-	);
-	
-	$(".previous").click(function() {
-		showImage(false);  
-	});
-
-	$(".previous").hover(
-		function(){
-			$(this).attr("src", "images/left_hover.png");
-		},
-		function(){
-			$(this).attr("src", "images/left.png");
-		}
-	);
-
-	$(".next").click(function() {
-		showImage(true);  
-	});
-		
-	$(".next").hover(
-		function(){
-			$(this).attr("src", "images/right_hover.png");
-		},
-		function(){
-			$(this).attr("src", "images/right.png");
-		}
-	);
-	
-	$(".bSelector a").click(function() {
-		showNextImage($('.bSelector a').index($(this)), true);
-	});
-
-	$(".bSelector a img").hover(function() {
-		var index = $(".bSelector a").index(this.parentElement);
-		$(".bSelector .preview").attr("src", images[index].src);
-		$(".bSelector .preview").show();
-		$(".labelImg").text(images[index].alt);
-	}, function() {
-		$(".bSelector .preview").hide();
-	});
-	
-	$(".labelImg").click(function(){
-		$("#bannerImage").click();
-	});
-	
-	$(".play").click(function() {
-		startBanner();
-		$(this).hide();
-		$(".stop").show();
-	});
-	
-	$(".stop").click(function() {
-		clearTimeout(timer);
-		$(this).hide();
-		$(".play").show();
-	});
-	
-	startBanner();*/
-});
-
-
-function Slider(speed) {
-	this.images = new Array();
-	this.i = 0;
+// Class Slider
+function Slider(speed, format, filename, data) {
+	this.images = new Array();	
 	this.speed = speed;
-	this.firstTime = true;	
-	//onLoaded();
-	//startBanner();
+	this.format = format;
+	this.filename = filename;
 }
-
-Slider.prototype.onLoaded = function() {
-	$("#bannerLink").hover(function() {
-			$(".bottomLabelImg").show();
-		},
-		function() {
-			$(".bottomLabelImg").hide();
-		}		
-	);
-	
-	$(".play").hover(
-		function(){
-			$(this).attr("src", "images/play_hover.png");
-		},
-		function(){
-			$(this).attr("src", "images/play.png");
-		}
-	);
-	
-	$(".previous").click(function() {
-		showImage(false);  
-	});
-
-	$(".previous").hover(
-		function(){
-			$(this).attr("src", "images/left_hover.png");
-		},
-		function(){
-			$(this).attr("src", "images/left.png");
-		}
-	);
-
-	$(".next").click(function() {
-		showImage(true);  
-	});
-		
-	$(".next").hover(
-		function(){
-			$(this).attr("src", "images/right_hover.png");
-		},
-		function(){
-			$(this).attr("src", "images/right.png");
-		}
-	);
-	
-	$(".bSelector a").click(function() {
-		showNextImage($('.bSelector a').index($(this)), true);
-	});
-
-	$(".bSelector a img").hover(function() {
-		var index = $(".bSelector a").index(this.parentElement);
-		$(".bSelector .preview").attr("src", images[index].src);
-		$(".bSelector .preview").show();
-		$(".labelImg").text(images[index].alt);
-	}, function() {
-		$(".bSelector .preview").hide();
-	});
-	
-	$(".labelImg").click(function(){
-		$("#bannerImage").click();
-	});
-	
-	$(".play").click(function() {
-		startBanner();
-		$(this).hide();
-		$(".stop").show();
-	});
-	
-	$(".stop").click(function() {
-		clearTimeout(timer);
-		$(this).hide();
-		$(".play").show();
-	});
-};
-
-Slider.prototype.startBanner = function() {
-	if (this.i == this.images.length)
-		this.i = 0;
-	showNextImage(this.i, false);
-	$("#bannerImage").fadeTo(1000, 1);
-	timer = setTimeout("startBanner();", this.speed);
-	$("#bannerImage").fadeTo(1500, 0.9);
-};
-
-Slider.prototype.showNextImage = function(index, keepShow) {
-	if (keepShow)
-		clearTimeout(timer);
-
-	this.i = this.i > this.images.length ? 0 : (this.i < 0 ? this.images.length - 1 : this.i);
-
-	$("#bannerImage").attr("src", this.images[index].src);
-	$("#bannerImage").attr("alt", this.images[index].alt);
-	$("#bannerLink").attr("href", this.images[index].link);
-	$(".bSelector a img").removeClass("selectedCircle");
-	$(".bSelector a img:eq(" + (index) + ")").attr("class", "selectedCircle");
-	$(".bottomLabelImg").text(this.images[index].alt);
-	if (keepShow) {
-		this.i = index;
-		timer = setTimeout("startBanner();", this.speed);
-	} else
-		this.i++;
-	showAndHideControls();
-};
-
-Slider.prototype.showImage = function(next) {
-	var newIndex = next ? this.i + 1 : this.i - 1;
-	if (this.firstTime) {
-		--newIndex;
-		this.firstTime = false;
-	}
-	newIndex = newIndex < 0 ? this.images.length - 1 : (newIndex >= this.images.length ? 0 : newIndex);
-	showNextImage(newIndex, true);
-	showAndHideControls();
-	return false;
-};
-
-Slider.prototype.showAndHideControls = function(){
-	$(".stop").show();
-	$(".play").hide();
-};
-
-Slider.prototype.loadImagesData = function(data, type) {
-	type = type == undefined ? "json" : type;
+// Functions of Slider
+Slider.prototype.loadImagesData = function() {	
 	try {
-		if(type == "json")
-			this.images = $.parseJSON(data).images;
-		else
-			return "Type format not supported";
+		if(this.data != undefined && this.type == 'json') {
+			if(type == "json")
+				this.images = $.parseJSON(this.data).images;
+		} else {
+			var imgs = new Array();
+			var format = this.format;
+			$.ajax({
+				type: "GET",
+				url: this.filename,
+				dataType: format,
+				success: function(result){
+					if(format == "xml") {						
+						$(result).find('image').each(function(){
+							var image = new Image($(this).find('src').text(), $(this).find('alt').text(), $(this).find('link').text());
+							imgs.push(image);
+						});
+					} else if(format == "json") {						
+						$(result.images).each(function() {
+							imgs.push(this);
+						});
+					}					
+				},
+				error: function() {
+					return "An error occurred while processing " + format + " file.";
+				}				
+			});
+			this.images = imgs;
+		}
 	} catch(err) {
 		return err;
 	}
 	return true;
 };
+
+// Slider code
+var i = 0;
+var firstTime = true;
+
+function startBanner() {
+	if (i == s.images.length)
+		i = 0;
+	showNextImage(i, false);
+	$("#bannerImage").fadeTo(1000, 1);
+	timer = setTimeout("startBanner();", s.speed);
+	$("#bannerImage").fadeTo(1500, 0.9);
+};
+
+function showNextImage(index, keepShow) {
+	if (keepShow)
+		clearTimeout(timer);
+
+	i = i > s.images.length ? 0 : (i < 0 ? s.images.length - 1 : i);
+
+	$("#bannerImage").attr("src", s.images[index].src);
+	$("#bannerImage").attr("alt", s.images[index].alt);
+	$("#bannerLink").attr("href", s.images[index].link);
+	$(".bSelector a img").removeClass("selectedCircle");
+	$(".bSelector a img:eq(" + (index) + ")").attr("class", "selectedCircle");
+	$(".bottomLabelImg").text(s.images[index].alt);
+	if (keepShow) {
+		i = index;
+		timer = setTimeout("startBanner();", s.speed);
+	} else
+		i++;
+	showAndHideControls();
+};
+
+function showImage(next) {
+	var newIndex = next ? i + 1 : i - 1;
+	if (firstTime) {
+		--newIndex;
+		firstTime = false;
+	}
+	newIndex = newIndex < 0 ? s.images.length - 1 : (newIndex >= s.images.length ? 0 : newIndex);
+	showNextImage(newIndex, true);
+	showAndHideControls();
+	return false;
+};
+
+function showAndHideControls(){
+	$(".stop").show();
+	$(".play").hide();
+};
+
+$(document).ready(function() {	
+	s.loadImagesData("xml", "imagesSource.xml");
+	
+	$("#bannerLink").hover(function() {
+			$(".bottomLabelImg").show();
+		},
+		function() {
+			$(".bottomLabelImg").hide();
+		}		
+	);
+	
+	$(".play").hover(
+		function(){
+			$(this).attr("src", "images/play_hover.png");
+		},
+		function(){
+			$(this).attr("src", "images/play.png");
+		}
+	);
+	
+	$(".previous").click(function() {
+		showImage(false);  
+	});
+
+	$(".previous").hover(
+		function(){
+			$(this).attr("src", "images/left_hover.png");
+		},
+		function(){
+			$(this).attr("src", "images/left.png");
+		}
+	);
+
+	$(".next").click(function() {
+		showImage(true);  
+	});
+		
+	$(".next").hover(
+		function(){
+			$(this).attr("src", "images/right_hover.png");
+		},
+		function(){
+			$(this).attr("src", "images/right.png");
+		}
+	);
+	
+	$(".bSelector a").click(function() {
+		showNextImage($('.bSelector a').index($(this)), true);
+	});
+
+	$(".bSelector a img").hover(function() {
+		var index = $(".bSelector a").index(this.parentElement);
+		$(".bSelector .preview").attr("src", s.images[index].src);
+		$(".bSelector .preview").show();
+		$(".labelImg").text(s.images[index].alt);
+	}, function() {
+		$(".bSelector .preview").hide();
+	});
+	
+	$(".labelImg").click(function(){
+		$("#bannerImage").click();
+	});
+	
+	$(".play").click(function() {
+		startBanner();
+		$(this).hide();
+		$(".stop").show();
+	});
+	
+	$(".stop").click(function() {
+		clearTimeout(timer);
+		$(this).hide();
+		$(".play").show();
+	});
+	
+	setTimeout("startBanner();", 30);
+});
